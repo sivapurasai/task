2be1f70e88d749bc683e339a7835771422c9ba26
sivapurasai <sivatrichy142@gmail.com>
2025-02-20 19:48:21 +0530

    new Creation

--- src/org/demo/Test.java
+++ src/org/demo/Test.java
@@ -2,14 +2,18 @@ package org.demo;
 
 public class Test {
 
-	public void method(int i) {
+	public void demoMeth(int i) {
 
 		System.out.println("sample  Test");
 		System.out.println("String"+ i);
 	}
 
-	private void method1() {
+	private void method2() {
 		System.out.println("sample Test 1");
+	}
+	
+	private void method3(String s) {
+		System.out.println("sample Test 1"+ s);
 	}
 
 }
